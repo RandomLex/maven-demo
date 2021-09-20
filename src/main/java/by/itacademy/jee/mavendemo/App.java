cb7ac3ad99cd78e9ac34bf9f5040177e156e28da
Alexej_Barzykin <Alexej_Barzykin@epam.com>
2021-09-20 21:13:34 +0300

    maven initial

---
+++ src/main/java/by/itacademy/jee/mavendemo/App.java
@@ -0,0 +1,11 @@
+package by.itacademy.jee.mavendemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
