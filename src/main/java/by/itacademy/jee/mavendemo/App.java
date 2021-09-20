abb61ea44b87174c624d8adcc60edab41263bd86
Alexej_Barzykin <Alexej_Barzykin@epam.com>
2021-09-20 21:53:36 +0300

    03-junit-demo

--- src/main/java/by/itacademy/jee/mavendemo/App.java
+++ src/main/java/by/itacademy/jee/mavendemo/App.java
@@ -1,11 +1,17 @@
 package by.itacademy.jee.mavendemo;
 
 /**
- * Hello world!
+ * Calculator
  *
  */
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        System.out.println(sum(3, 4));
     }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+
 }
--- src/test/java/by/itacademy/jee/mavendemo/AppTest.java
+++ src/test/java/by/itacademy/jee/mavendemo/AppTest.java
@@ -1,20 +1,35 @@
 package by.itacademy.jee.mavendemo;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
- * Unit test for simple App.
+ * Unit test for App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void sumAPositiveBPositive() {
+        //given
+        int a = 5;
+        int b = 10;
+        int expectedSum = 15;
+        //when
+        int actualSum = App.sum(a, b);
+        //then
+        assertEquals(expectedSum, actualSum);
+    }
+
+    @Test
+    public void sumAPositiveMaximaBPositive() {
+        //given
+        int a = Integer.MAX_VALUE;
+        int b = 10;
+        int expectedSum = -2147483639;
+        //when
+        int actualSum = App.sum(a, b);
+        //then
+        assertEquals(expectedSum, actualSum);
     }
 }
